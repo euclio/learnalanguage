59b934f08db21cff6c869fde65ee4ee0a6cbde5e
Andy Russell <arussell123@gmail.com>
2012-06-01 15:05:02 -0700

    The compile button now invokes a compiler on the given source.

--- src/LearningGUI.java
+++ src/LearningGUI.java
@@ -4,8 +4,11 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.EventListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -18,13 +21,15 @@ import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class LearningGUI extends JFrame {
+    private final Compiler compiler = new Compiler();
+    
     private static final int EDITOR_COLS = 80;
     private static final int EDITOR_ROWS = 80;
     private static final Font EDITOR_FONT = new Font("Monospaced", Font.PLAIN, 14);
     
     private static final int TERMINAL_COLS = 60;
     private static final int TERMINAL_ROWS = 4;
-    private static final String TERMINAL_TEXT = "Welcome to LearnALanguage";
+    private static final String TERMINAL_TEXT = "Welcome to LearnALanguage\n";
     private static final Font TERMINAL_FONT = new Font("Monospaced",
             Font.PLAIN, 12);
 
@@ -37,6 +42,11 @@ public class LearningGUI extends JFrame {
         JPanel optionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
         JButton compileButton = new JButton("Compile");
+        compileButton.addActionListener(new ActionListener() {
+           public void actionPerformed(ActionEvent e) {
+               compiler.compile("Test.java");
+           }
+        });
         optionsPanel.add(compileButton);
         
         JButton run = new JButton("Run!");
@@ -47,6 +57,7 @@ public class LearningGUI extends JFrame {
         editor.setContentType("text/java");
         JScrollPane editorPane = new JScrollPane(editor);
         editorPane.setBorder(BorderFactory.createTitledBorder("Editor"));
+        compiler.setSource(editor);
 
         // Create the panel that holds the console  
         JTextArea console = new JTextArea(TERMINAL_TEXT, TERMINAL_ROWS, TERMINAL_COLS);
@@ -54,6 +65,9 @@ public class LearningGUI extends JFrame {
         console.setFont(TERMINAL_FONT);
         JScrollPane consolePane = new JScrollPane(console);
         consolePane.setBorder(BorderFactory.createTitledBorder("Console"));
+        compiler.setOutput(console);
+        
+
 
         // Add the components to the main window
         this.setJMenuBar(new LearningMenuBar());
