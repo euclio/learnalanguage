da674e3c1757d0e58d1511e234118b33a2e54ee6
Andy Russell <arussell123@gmail.com>
2012-05-31 17:32:38 -0700

    Completed the basic layout of the GUI

--- src/LearningGUI.java
+++ src/LearningGUI.java
@@ -1,43 +1,64 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
-
 
 @SuppressWarnings("serial")
 public class LearningGUI extends JFrame {
-    private static final int TERMINAL_COLS = 40;
-    private static final int TERMINAL_ROWS = 80;
+    private static final int EDITOR_COLS = 80;
+    private static final int EDITOR_ROWS = 80;
+    private static final Font EDITOR_FONT = new Font("Monospaced", Font.PLAIN, 14);
+    
+    private static final int TERMINAL_COLS = 60;
+    private static final int TERMINAL_ROWS = 4;
     private static final String TERMINAL_TEXT = "Welcome to LearnALanguage";
-    private static final Font TERMINAL_FONT = new Font("Monospaced", Font.PLAIN, 12);
-    
-    private static final Dimension WINDOW_DIMENSION = new Dimension(800, 600);
-    
-    public LearningGUI (String title) {
-        super(title);        
+    private static final Font TERMINAL_FONT = new Font("Monospaced",
+            Font.PLAIN, 12);
+
+    private static final Dimension WINDOW_DIMENSION = new Dimension(1024, 768);
+
+    public LearningGUI(String title) {
+        super(title);
+
+        // Initialize the top panel
+        JPanel optionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        JButton compileButton = new JButton("Compile");
+        optionsPanel.add(compileButton);
         
-        JPanel optionsPanel = new JPanel();
+        JButton run = new JButton("Run!");
+        optionsPanel.add(run);
         
-        JPanel editorPanel = new JPanel();
-        
-        JPanel consolePanel = new JPanel();
-        
+        // Create the panel that holds the code editor
+        JEditorPane editor = new JEditorPane();
+        editor.setContentType("text/java");
+        JScrollPane editorPane = new JScrollPane(editor);
+        editorPane.setBorder(BorderFactory.createTitledBorder("Editor"));
+
+        // Create the panel that holds the console  
         JTextArea console = new JTextArea(TERMINAL_TEXT, TERMINAL_ROWS, TERMINAL_COLS);
-        console.setFont(TERMINAL_FONT);
-        consolePanel.add(console);
-        
         console.setEditable(false);
-        
+        console.setFont(TERMINAL_FONT);
+        JScrollPane consolePane = new JScrollPane(console);
+        consolePane.setBorder(BorderFactory.createTitledBorder("Console"));
+
+        // Add the components to the main window
         this.add(optionsPanel, BorderLayout.NORTH);
-        this.add(editorPanel, BorderLayout.CENTER);
-        this.add(consolePanel, BorderLayout.EAST);
+        this.add(editorPane, BorderLayout.CENTER);
+        this.add(consolePane, BorderLayout.EAST);
+        
+        // Final Options for GUI
         this.setSize(WINDOW_DIMENSION);
-        
-        
     }
 }
