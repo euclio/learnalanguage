1521fd980b1b0db679a2f4494405bc1a0b1a660e
Andy Russell <arussell123@gmail.com>
2012-06-01 17:32:30 -0700

    Added ability to run compiled classes from the GUI

--- src/LearningGUI.java
+++ src/LearningGUI.java
@@ -1,14 +1,13 @@
 import java.awt.BorderLayout;
-import java.awt.Container;
+
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridBagLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.EventListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -16,17 +15,12 @@ import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class LearningGUI extends JFrame {
-    private final Compiler compiler = new Compiler();
-    
-    private static final int EDITOR_COLS = 80;
-    private static final int EDITOR_ROWS = 80;
-    private static final Font EDITOR_FONT = new Font("Monospaced", Font.PLAIN, 14);
-    
+    private final Compiler compiler;
+    private final Runner runner;
+
     private static final int TERMINAL_COLS = 60;
     private static final int TERMINAL_ROWS = 4;
     private static final String TERMINAL_TEXT = "Welcome to LearnALanguage\n";
@@ -34,6 +28,9 @@ public class LearningGUI extends JFrame {
             Font.PLAIN, 12);
 
     private static final Dimension WINDOW_DIMENSION = new Dimension(1024, 768);
+
+    private String className = "Test";
+    private String[] args = new String[0];
 
     public LearningGUI(String title) {
         super(title);
@@ -43,42 +40,46 @@ public class LearningGUI extends JFrame {
 
         JButton compileButton = new JButton("Compile");
         compileButton.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent e) {
-               compiler.compile("Test.java");
-           }
+            public void actionPerformed(ActionEvent e) {
+                compiler.compile(className + ".java");
+            }
         });
         optionsPanel.add(compileButton);
-        
-        JButton run = new JButton("Run!");
-        optionsPanel.add(run);
-        
+
+        JButton runButton = new JButton("Run!");
+        runButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                runner.run(className, args);
+            }
+        });
+        optionsPanel.add(runButton);
+
         // Create the panel that holds the code editor
         JEditorPane editor = new JEditorPane();
         editor.setContentType("text/java");
         JScrollPane editorPane = new JScrollPane(editor);
         editorPane.setBorder(BorderFactory.createTitledBorder("Editor"));
-        compiler.setSource(editor);
 
-        // Create the panel that holds the console  
-        JTextArea console = new JTextArea(TERMINAL_TEXT, TERMINAL_ROWS, TERMINAL_COLS);
+        // Create the panel that holds the console
+        LearningConsole console = new LearningConsole(TERMINAL_TEXT,
+                TERMINAL_ROWS, TERMINAL_COLS);
         console.setEditable(false);
         console.setFont(TERMINAL_FONT);
+        console.setCaretPosition(console.getDocument().getLength());
         JScrollPane consolePane = new JScrollPane(console);
         consolePane.setBorder(BorderFactory.createTitledBorder("Console"));
-        compiler.setOutput(console);
-        
-
-
+        compiler = new Compiler(editor, console);
+        runner = new Runner(console);
         // Add the components to the main window
         this.setJMenuBar(new LearningMenuBar());
         this.add(optionsPanel, BorderLayout.NORTH);
         this.add(editorPane, BorderLayout.CENTER);
         this.add(consolePane, BorderLayout.EAST);
-        
+
         // Final Options for GUI
-        
+
         this.setSize(WINDOW_DIMENSION);
-        
+
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
