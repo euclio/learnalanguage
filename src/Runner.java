4c732d14d17f6cc9e8ff11a87dc46bb17666289f
Andy Russell <arussell123@gmail.com>
2012-06-04 09:53:35 -0700

    Now runs Java programs in a separate thread. Also cleaned up the try/catches and error handling.

--- src/Runner.java
+++ src/Runner.java
@@ -7,40 +7,63 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 public class Runner {
+    private class RunnerThread extends Thread {
+        private File f;
+        private Class<?> theClass;
+        private Method main;
+
+        private String[] parameters;
+
+        public RunnerThread(String className, String[] parameters) {
+            System.setOut(new PrintStream(output.getStream()));
+            System.setErr(new PrintStream(output.getStream()));
+            System.out.println("\nRunning " + className + ".java...");
+            System.out.println();
+
+            f = new File(System.getProperty("user.dir"));
+
+            URLClassLoader loader = null;
+            try {
+                loader = new URLClassLoader(new URL[] { f.toURI().toURL() });
+            } catch (MalformedURLException e) {
+                System.err
+                        .println("Error: There was an error finding the current directory.");
+            }
+
+            try {
+                theClass = loader.loadClass(className);
+            } catch (ClassNotFoundException e) {
+                System.err.println("Error: The class could not be found.");
+            }
+
+            try {
+                main = theClass.getMethod("main", String[].class);
+            } catch (NoSuchMethodException e) {
+                System.err.println("Error: 'main' method not found.");
+            }
+
+            this.parameters = parameters;
+        }
+
+        public void run() {
+            try {
+                main.invoke(null, new Object[] { parameters });
+            } catch (IllegalAccessException e) {
+                System.err.println("Error: Access to this method is denied.");
+            } catch (InvocationTargetException e) {
+                System.err.println("ERROR: The program terminated with:\n"
+                        + e.getMessage());
+            }
+        }
+    }
+
     private LearningConsole output;
 
     public Runner(LearningConsole output) {
         this.output = output;
     }
 
-    public void run(String className, String[] parameters) {
-        try {
-            System.setOut(new PrintStream(output.getStream()));
-            System.setErr(new PrintStream(output.getStream()));
-            System.out.println("\nRunning " + className + ".java...");
-            System.out.println();
-            URLClassLoader loader = null;
-            File f = new File(System.getProperty("user.dir"));
-            try {
-                loader = new URLClassLoader(new URL[] { f.toURI().toURL() });
-            } catch (MalformedURLException e1) {
-                System.out.println("ERROR: File not found");
-            }
-            Class<?> theClass = loader.loadClass(className);
-            try {
-                Method main = theClass.getMethod("main", String[].class);
-                try {
-                    main.invoke(null, new Object[] { parameters });
-                } catch (IllegalAccessException e) {
-                    System.err.println("ERROR: " + e.getMessage());
-                } catch (InvocationTargetException e) {
-                    System.err.println("ERROR: " + e.getMessage());
-                }
-            } catch (NoSuchMethodException e) {
-                System.err.println("ERROR: " + e.getMessage());
-            }
-        } catch (ClassNotFoundException e) {
-            System.err.println("ERROR: " + e.getMessage());
-        }
+    public void execute(String className, String[] parameters) {
+        new RunnerThread(className, parameters).start();
     }
 }
