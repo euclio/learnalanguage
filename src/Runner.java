e129abc04010291e8da08fa677870d496a7f341e
Andy Russell <arussell123@gmail.com>
2012-06-04 14:50:26 -0700

    Major changes in implementation.  The program to be executed now runs in its own process, which can be forcibly terminatd with stop().

--- src/Runner.java
+++ src/Runner.java
@@ -1,69 +1,52 @@
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 public class Runner {
-    private class RunnerThread extends Thread {
-        private File f;
-        private Class<?> theClass;
-        private Method main;
-
-        private String[] parameters;
-
-        public RunnerThread(String className, String[] parameters) {
-            System.setOut(new PrintStream(output.getStream()));
-            System.setErr(new PrintStream(output.getStream()));
-            System.out.println("\nRunning " + className + ".java...");
-            System.out.println();
-
-            f = new File(System.getProperty("user.dir"));
-
-            URLClassLoader loader = null;
-            try {
-                loader = new URLClassLoader(new URL[] { f.toURI().toURL() });
-            } catch (MalformedURLException e) {
-                System.err
-                        .println("Error: There was an error finding the current directory.");
-            }
-
-            try {
-                theClass = loader.loadClass(className);
-            } catch (ClassNotFoundException e) {
-                System.err.println("Error: The class could not be found.");
-            }
-
-            try {
-                main = theClass.getMethod("main", String[].class);
-            } catch (NoSuchMethodException e) {
-                System.err.println("Error: 'main' method not found.");
-            }
-
-            this.parameters = parameters;
-        }
-
-        public void run() {
-            try {
-                main.invoke(null, new Object[] { parameters });
-            } catch (IllegalAccessException e) {
-                System.err.println("Error: Access to this method is denied.");
-            } catch (InvocationTargetException e) {
-                System.err.println("ERROR: The program terminated with:\n"
-                        + e.getMessage());
-            }
-        }
-    }
-
     private LearningConsole output;
-
+    private Process p;
     public Runner(LearningConsole output) {
         this.output = output;
     }
+    
+    class StreamPiper extends Thread {
+             InputStream is;
+        StreamPiper(InputStream is) {
+            this.is = is;
+        }
+        
+        public void run() {
+            try {
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String line;
+                while((line = br.readLine()) != null)
+                    System.out.println(line);
+            
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+    }
 
     public void execute(String className, String[] parameters) {
-        new RunnerThread(className, parameters).start();
+        ProcessBuilder pb = new ProcessBuilder("java", className);
+        System.setOut(new PrintStream(output.getStream()));
+        System.setErr(new PrintStream(output.getStream()));
+        System.out.println("\nRunning " + className + ".java...");
+        System.out.println();
+        try {
+            p = pb.start();
+        } catch (IOException e) {
+            System.out.println("Error in execution: " + e.getMessage());
+        }
+        StreamPiper output = new StreamPiper(p.getInputStream());
+        output.start();
+    }
+    
+    public void stop() {
+        p.destroy();
     }
 }
