6f93b11a329b7dadf2f1bbf65cc7c055ed1d4fb8
Andy Russell <arussell123@gmail.com>
2012-06-05 08:45:00 -0700

    Sped up the writing of text to the JTextArea and (sort of) fixed the GUI hangs if there is a large amount of text written to the JTextArea at once.

--- src/LearningConsole.java
+++ src/LearningConsole.java
@@ -1,19 +1,35 @@
 import java.io.OutputStream;
 
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import javax.swing.text.DefaultCaret;
 
 @SuppressWarnings("serial")
 public class LearningConsole extends JTextArea {
     private OutputStream writer = new OutputStream() {
+        StringBuffer buff = new StringBuffer();
+
         public void write(int b) {
-            LearningConsole.this.append(String.valueOf((char) b));
+            if ((char) b == '\r')
+                return;
+            if ((char) b == '\n') {
+                final String text = buff.toString() + '\n';
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        LearningConsole.this.append(text);
+                    }
+                });
+                buff.setLength(0);
+                return;
+            }
+
+            buff.append((char) b);
         }
     };
 
     public LearningConsole(String text, int rows, int cols) {
         super(rows, cols);
-        DefaultCaret caret = (DefaultCaret)this.getCaret();
+        DefaultCaret caret = (DefaultCaret) this.getCaret();
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         this.append(text);
     }
