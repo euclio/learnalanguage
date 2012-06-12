d7e692ba9b277d2ff9b266f27908e702fec42fb9
Andy Russell <arussell123@gmail.com>
2012-06-12 14:01:38 -0500

    Readability improvements.

--- src/LearningGUI.java
+++ src/LearningGUI.java
@@ -2,15 +2,10 @@ import java.awt.BorderLayout;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
@@ -20,19 +15,26 @@ import javax.swing.JScrollPane;
 
 @SuppressWarnings("serial")
 public class LearningGUI extends JFrame {
+    private final JPanel optionsPanel, consoleContainer;
+    private final JButton compileButton, runButton, stopButton;
+    private final JEditorPane editor;
+    private final JScrollPane editorScroll, terminalScroll;
+    private final Console terminal, status;
+
     private final Compiler compiler;
     private final Runner runner;
 
     private static final int TERMINAL_COLS = 60;
     private static final int TERMINAL_ROWS = 4;
+    private static final int STATUS_COLS = TERMINAL_COLS;
+    private static final int STATUS_ROWS = 8;
     private static final String TERMINAL_TEXT = "Welcome to LearnALanguage\n";
-    private static final Font TERMINAL_FONT = new Font("Monospaced",
-            Font.PLAIN, 12);
 
     private static final Dimension WINDOW_DIMENSION = new Dimension(1024, 768);
 
     private String className = "Test";
     private String[] args = new String[0];
+    // Simple 'Hello World' program
     private String initialProgram = "public class Test {\n"
             + "\tpublic static void main(String [] args) {\n"
             + "\t\tSystem.out.println(\"Hello World!\");\n" + "\t}\n" + "}";
@@ -41,11 +43,11 @@ public class LearningGUI extends JFrame {
         super(title);
 
         // Initialize the top panel
-        JPanel optionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        optionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        final JButton compileButton = new JButton("Compile");
-        final JButton runButton = new JButton("Run!");
-        final JButton stopButton = new JButton("Stop");
+        compileButton = new JButton("Compile");
+        runButton = new JButton("Run!");
+        stopButton = new JButton("Stop");
 
         compileButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -57,25 +59,26 @@ public class LearningGUI extends JFrame {
         runButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 runner.execute(className, args);
-                //stopButton.setEnabled(true);
             }
         });
         optionsPanel.add(runButton);
 
         stopButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                runner.stop();
-                //((JButton) e.getSource()).setEnabled(false);
+                runner.kill();
             }
         });
 
+        // Thread that toggles buttons if the machine is executing
         (new Thread() {
             public void run() {
                 while (true) {
                     if (runner != null && runner.isExecuting()) {
                         stopButton.setEnabled(true);
+                        compileButton.setEnabled(false);
                     } else {
                         stopButton.setEnabled(false);
+                        compileButton.setEnabled(true);
                     }
                 }
             }
@@ -85,30 +88,39 @@ public class LearningGUI extends JFrame {
 
         // Create the panel that holds the code editor
         jsyntaxpane.DefaultSyntaxKit.initKit();
-        JEditorPane editor = new JEditorPane();
-        JScrollPane editorPane = new JScrollPane(editor);
+        editor = new JEditorPane();
+
+        editorScroll = new JScrollPane(editor);
+        editorScroll.setBorder(BorderFactory.createTitledBorder("Editor"));
+
         editor.setContentType("text/java");
         editor.setText(initialProgram);
-        editorPane.setBorder(BorderFactory.createTitledBorder("Editor"));
 
-        // Create the panel that holds the console
-        LearningConsole console = new LearningConsole(TERMINAL_TEXT,
-                TERMINAL_ROWS, TERMINAL_COLS);
-        console.setEditable(false);
-        console.setFont(TERMINAL_FONT);
-        JScrollPane consolePane = new JScrollPane(console);
-        consolePane.setBorder(BorderFactory.createTitledBorder("Console"));
-        compiler = new Compiler(editor);
-        runner = new Runner();
-        
-        System.setOut(new PrintStream(console.getStream()));
-        System.setErr(new PrintStream(console.getStream()));
+        // Create the panel that holds the consoles
+        consoleContainer = new JPanel(new BorderLayout());
+
+        terminal = new Console(TERMINAL_TEXT, TERMINAL_ROWS, TERMINAL_COLS);
+
+        terminalScroll = new JScrollPane(terminal);
+        terminalScroll.setBorder(BorderFactory.createTitledBorder("Terminal"));
+
+        consoleContainer.add(terminalScroll, BorderLayout.CENTER);
+
+        status = new Console(STATUS_ROWS, STATUS_COLS);
+        status.setMaxLines(STATUS_ROWS);
+
+        status.setBorder(BorderFactory.createTitledBorder("VM Status"));
+
+        consoleContainer.add(status, BorderLayout.SOUTH);
+
+        compiler = new Compiler(editor, status);
+        runner = new Runner(terminal, status);
 
         // Add the components to the main window
         this.setJMenuBar(new LearningMenuBar());
         this.add(optionsPanel, BorderLayout.NORTH);
-        this.add(editorPane, BorderLayout.CENTER);
-        this.add(consolePane, BorderLayout.EAST);
+        this.add(editorScroll, BorderLayout.CENTER);
+        this.add(consoleContainer, BorderLayout.EAST);
 
         // Final Options for GUI
         this.setSize(WINDOW_DIMENSION);
