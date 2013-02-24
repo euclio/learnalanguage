e39ec6a31c5b961e955c5914a006d10ea04ce224
Andy Russell <arussell123@gmail.com>
2013-02-24 02:22:46 -0800

    now functions as a collection of constants of possible preferences

--- src/com/acrussell/learnalanguage/Settings.java
+++ src/com/acrussell/learnalanguage/Settings.java
@@ -1,11 +1,7 @@
 package com.acrussell.learnalanguage;
 
-import java.util.prefs.Preferences;
-
 public class Settings {
-    private static Preferences prefs = Preferences.userRoot();
-    
-    public static Preferences getSettings() {
-        return prefs;
-    }
+    public static final String TERM_SHOULD_CLEAR = "terminal_should_clear";
+    public static final String LAST_OPEN_FILE = "last_file";
+    public static final String OPEN_FILES = "open_files";
 }
