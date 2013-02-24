8a25ba5ccbfa7bad52b9b7bd993ccc7d5b3d0029
Andy Russell <arussell123@gmail.com>
2013-02-24 02:28:59 -0800

    added menu option to open a file

--- src/com/acrussell/learnalanguage/LearningMenuBar.java
+++ src/com/acrussell/learnalanguage/LearningMenuBar.java
@@ -2,19 +2,25 @@ package com.acrussell.learnalanguage;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.prefs.Preferences;
 
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 @SuppressWarnings("serial")
 public class LearningMenuBar extends JMenuBar {
+    private LearningGUI gui;
+    private Preferences prefs = Preferences.userNodeForPackage(this.getClass());
+
     public enum MenuChoices {
         // File
-        EXIT,
+        OPEN, EXIT,
 
         // Help
         ABOUT
@@ -27,6 +33,10 @@ public class LearningMenuBar extends JMenuBar {
             String itemText = source.getText().replaceAll("[^a-zA-Z0-9]", "");
 
             switch (MenuChoices.valueOf(itemText.toUpperCase())) {
+            case OPEN:
+                open(gui.getEditorPanel());
+                break;
+
             case EXIT:
                 exit();
                 break;
@@ -38,14 +48,19 @@ public class LearningMenuBar extends JMenuBar {
         }
     }
 
-    private Preferences prefs = Preferences.userRoot();
-
-    public LearningMenuBar() {
+    public LearningMenuBar(LearningGUI gui) {
+        this.gui = gui;
         ActionListener menuListener = new MenuListener();
 
         // Build the "File" Menu
         JMenu fileMenu = new JMenu("File");
 
+        // Open file option
+        JMenuItem open = new JMenuItem("Open...");
+        open.addActionListener(menuListener);
+        fileMenu.add(open);
+
+        // Add the exit option
         JMenuItem close = new JMenuItem("Exit");
         close.addActionListener(menuListener);
         fileMenu.add(close);
@@ -60,10 +75,10 @@ public class LearningMenuBar extends JMenuBar {
         clearTerminal.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JCheckBoxMenuItem source = (JCheckBoxMenuItem) e.getSource();
-                Preferences.userNodeForPackage(this.getClass()).putBoolean(
-                        "terminal_should_clear", source.isSelected());
+                prefs.putBoolean(Settings.TERM_SHOULD_CLEAR, source.isSelected());
             }
         });
+        clearTerminal.setSelected(prefs.getBoolean(Settings.TERM_SHOULD_CLEAR, false));
         optionsMenu.add(clearTerminal);
         this.add(optionsMenu);
 
@@ -75,6 +90,16 @@ public class LearningMenuBar extends JMenuBar {
         helpMenu.add(about);
 
         this.add(helpMenu);
+    }
+
+    public void open(EditorPanel ep) {
+        JFileChooser chooser = new JFileChooser(new File(""));
+        chooser.setFileFilter(new FileNameExtensionFilter("Java Files", "java"));
+        int returnVal = chooser.showOpenDialog(chooser.getParent());
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            ep.addFile(chooser.getSelectedFile());
+        }
     }
 
     public void about() {
